ed19223e7a8be8a14fc0fa6a2692edfd743f86d2
jcaustin14 <jcaustin14@gmail.com>
2023-06-08 18:43:43 -0500

    added buy modal and search bar to nav

--- src/Components/BuyStock.js
+++ src/Components/BuyStock.js
@@ -10,13 +10,31 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
 
 const BuyStock = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate()
   const [quantity, setQuantity] = useState(1);
   const [totalValue, setTotalValue] = useState(0);
-  const [tradingFunds, setTradingFunds] = useState('')
+  const [tradingFunds, setTradingFunds] = useState('');
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  
   
   const { ticker } = useParams();
   
@@ -54,47 +72,49 @@ const BuyStock = () => {
     
   };
 
-
-
-   
-  
-
   return (
-    // <form>
-    //   <label>Quantity:</label>
-    //   <TextField onChange={ ev => update(ev.target.value) } defaultValue={ quantity } type='number'></TextField>
-    //   <label>Current Price:</label>
-    //   <div>{ stock.currentPrice }</div>
-    //   <label>Available Cash:</label>
-    //   <div>This comes from the user's info</div>
-    //   <label>Total Value:</label>
-    //   <div>{ totalValue.toFixed(2) }</div>
-    //   <Button onClick={ buy }>Buy { ticker }</Button>
-    // </form>
-    <form>
+    <div>
 
-      <Card sx={{ width: 300 }}>
-      <CardContent>
-        <Typography style={{display: 'flex', justifyContent: 'center'}} sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          { ticker }
-        </Typography>
-        <Typography variant="h5" component="div">
-          Current Price: { stock.currentPrice }
-        </Typography>
-        <div style={{ marginBottom: 8 }}/>
-        <TextField style={{ width: 200}} label='Shares' onChange={ ev => update(ev.target.value) } defaultValue={ quantity } type='number'></TextField>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Total Value: { totalValue.toFixed(2) }
-        </Typography>
-        <Typography variant="body2">
-          Available Funds: { auth.tradingFunds }
-        </Typography>
-      </CardContent>
-      <CardActions style={{display: 'flex', justifyContent: 'center'}}>
-        <Button onClick={ buy }>Buy { ticker }</Button>
-      </CardActions>
-    </Card>
-    </form>
+
+      <div>
+        <Button onClick={handleOpen}>Buy Modal</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="buy-stock"
+          aria-describedby="buy-stock-description"
+        >
+          <Box sx={style}>
+            {/* <Typography id="buy-stock-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="buy-stock-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography> */}
+            <form>
+                <Typography style={{display: 'flex', justifyContent: 'center'}} sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                  { ticker }
+                </Typography>
+                <Typography variant="h5" component="div">
+                  Current Price: { stock.currentPrice }
+                </Typography>
+                  <div style={{ marginBottom: 8 }}/>
+                <TextField style={{ width: 200}} label='Shares' onChange={ ev => update(ev.target.value) } defaultValue={ quantity } type='number'></TextField>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  Total Value: { totalValue.toFixed(2) }
+                </Typography>
+                <Typography variant="body2">
+                  Available Funds: { auth.tradingFunds }
+                </Typography>
+                <CardActions style={{display: 'flex', justifyContent: 'center'}}>
+                  <Button onClick={ buy }>Buy { ticker }</Button>
+                </CardActions>
+            </form>
+          </Box>
+        </Modal>
+      </div>
+    </div>
+    
   );
 };
 
