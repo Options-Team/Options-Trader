f7b606265d2c7f2e67131ff52dbc75df97cc89aa
wubwub98 <wahabmc2000@gmail.com>
2023-06-12 16:21:49 -0500

    changed back to linux conn

--- server/db/conn.js
+++ server/db/conn.js
@@ -6,8 +6,8 @@ if(process.env.QUIET){
   config.logging = false;
 }
 
-// const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/options_trader_db', config);
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:abcde@localhost:5432/options_trader_db', config);
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/options_trader_db', config);
+// const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:abcde@localhost:5432/options_trader_db', config);
 
 //line 9 is for Wahab's windows local machine. comment it out and use line 8.
 
